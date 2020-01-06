f5e9e097d87f46c86ad4c9b10892803f4df2a818
TurKurT656 <turkurt656@gmail.com>
2020-01-06 18:27:03 +0330

    cleared extra publishing tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,33 +3,7 @@ version = "0.1.0"
 
 plugins {
     `build-scan`
-    `maven-publish`
     kotlin("jvm") version "1.3.60"
-    id("org.jetbrains.dokka") version "0.9.17"
-    id("com.jfrog.bintray") version "1.8.4"
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("default") {
-            from(components["java"])
-        }
-    }
-    repositories {
-        maven {
-            url = uri("$buildDir/repository")
-        }
-    }
-}
-
-tasks {
-    dokka {
-        outputFormat = "html"
-        outputDirectory = "$buildDir/javadoc"
-    }
-    clean {
-        rootProject.buildDir
-    }
 }
 
 repositories {
