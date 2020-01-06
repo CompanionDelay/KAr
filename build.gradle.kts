0f16254f393564060bfd1dd4f5225b5e04b63272
TurKurT656 <turkurt656@gmail.com>
2020-01-06 21:45:02 +0330

    implemented jitpack repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,18 +8,13 @@ plugins {
 
 repositories {
     jcenter()
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
     implementation(kotlin("stdlib", "1.3.61"))
     testImplementation("junit:junit:4.12")
-}
-
-buildScan {
-    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-    termsOfServiceAgree = "yes"
-
-    publishAlways()
+    implementation("com.github.CompanionDelay:KAr:$version")
 }
 
 tasks {
