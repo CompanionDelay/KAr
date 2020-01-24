5357937f000e91304942458c31b893728c65ec4e
TurKurT656 <turkurt656@gmail.com>
2020-01-24 14:40:41 +0330

    removed test func

--- src/main/kotlin/com/github/companiondelay/core/KAr.kt
+++ src/main/kotlin/com/github/companiondelay/core/KAr.kt
@@ -1,5 +1,5 @@
 package com.github.companiondelay.core
 
 class KAr {
-    fun libName() = "KAr"
+
 }
--- src/test/kotlin/com/github/companiondelay/KArTest.kt
+++ src/test/kotlin/com/github/companiondelay/KArTest.kt
@@ -1,12 +1,11 @@
 package com.github.companiondelay
 
-import com.github.companiondelay.core.KAr
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class KArTest {
     @Test
     fun testMyLanguage() {
-        assertEquals("KAr", KAr().libName())
+        assertEquals(true, true)
     }
 }
