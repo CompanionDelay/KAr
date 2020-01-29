f3652ad7bb21bf60554eec223d776b461a85345f
TurKurT656 <turkurt656@gmail.com>
2020-01-29 14:30:56 +0330

    added kArDate as a property in the KAr class to have core functionality also added invoke func to help initializing KAr date through this func only

--- src/main/kotlin/com/github/companiondelay/core/KAr.kt
+++ src/main/kotlin/com/github/companiondelay/core/KAr.kt
@@ -2,4 +2,9 @@ package com.github.companiondelay.core
 
 class KAr private constructor() {
 
+    internal val kArDate: KArDate = KArDate()
+
+    companion object {
+        internal operator fun invoke() = KAr()
+    }
 }
